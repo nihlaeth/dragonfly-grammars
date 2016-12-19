4ee61024b8573e8e1c77528983c3bf08e8a59b80
nihlaeth <info@nihlaeth.nl>
2016-12-19 18:40:16 +0100

    install _ in common instead of as builtin

---
+++ dragonfly_grammars/common.py
@@ -0,0 +1,9 @@
+"""Common values and functions for dragonfly_grammars."""
+_GETTEXT_FUNC = lambda text: None
+# pylint: disable=unnecessary-lambda
+_ = lambda text: _GETTEXT_FUNC(text)
+
+def set_translator(gettext_function):
+    """Change translatorfunc (language change)."""
+    global _GETTEXT_FUNC
+    _GETTEXT_FUNC = gettext_function
