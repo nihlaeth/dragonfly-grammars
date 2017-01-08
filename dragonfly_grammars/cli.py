785e1db5b68b5d0e68aea80161aff651a9e6f2da
nihlaeth <info@nihlaeth.nl>
2017-01-08 19:58:09 +0100

    try matching on class

--- dragonfly_grammars/cli.py
+++ dragonfly_grammars/cli.py
@@ -131,7 +131,7 @@ def load():
     """Register grammar."""
     global GRAMMAR
     GRAMMAR = Grammar('command_line_interface', context=(
-        AppContext('putty') | ProxyAppContext(executable='terminator')))
+        AppContext('putty') | ProxyAppContext(cls='terminator')))
     GRAMMAR.add_rule(SshRule())
     GRAMMAR.add_rule(SimpleCommand())
     GRAMMAR.load()
