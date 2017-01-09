123821aeeb8822b8f42b48b788a305894ade7878
nihlaeth <info@nihlaeth.nl>
2017-01-09 18:56:17 +0100

    wip: expand vim grammar

--- dragonfly_grammars/vim.py
+++ dragonfly_grammars/vim.py
@@ -6,7 +6,8 @@ Vim grammar.
 Rules for everything vim and vim-like. Actually consists
 of multiple grammars for context purposes.
 
-TODO: marks, symbols and digits, repetition, window mode
+TODO: window mode, :commands, rest of normal keybindings, filters
+    other modes, vi-like mode, pentadactyl bindings, plugins, dutch translations
 """
 
 from aenea import (
@@ -23,7 +24,11 @@ from aenea import (
     Key)
 
 from dragonfly_grammars.common import _, extract_values
-from dragonfly_grammars.global_ import Number
+from dragonfly_grammars.global_ import Number, AnyCharacter
+
+##################################
+#  Rules with multiple purposes  #
+##################################
 
 class MotionRule(MappingRule):
 
@@ -38,17 +43,17 @@ class MotionRule(MappingRule):
             ################
             _("(backward|left)"): "h",
             _("(forward|right)"): "l",
-            _("(zero|first char[acter])"): "zero",
+            _("(zero|first char[acter])"): "0",
             _("(caret|first non-blank char[acter])"): "caret",
             _("(dollar|last char[acter])"): "dollar",
             _("last visible non-blank char[acter]"): "g,underscore",
-            _("fist visible char[acter]"): "g,zero",
+            _("fist visible char[acter]"): "g,0",
             _("first visible non-blank char[acter]"): "g,caret",
             _("middle of line"): "g,m",
             _("last visible char[acter]"): "g,dollar",
             _("(pipe|column)"): "bar",
-            # _("find <char>"): "f",
-            # _("backwards find <char>"): "s-f",
+            _("find <char>"): "f,%(char)s",
+            _("backwards find <char>"): "s-f,%(char)s",
             #############
             #  up/down  #
             #############
@@ -59,7 +64,7 @@ class MotionRule(MappingRule):
             _("(minus|linewise non-blank up)"): "minus",
             _("(plus|linewise non-blank down)"): "plus",
             _("(underscore|first non-blank line down)"): "underscore",
-            _("goto"): "s-g",
+            _("line"): "s-g",
             _("end of [last] line"): "c-end",
             _("first non-blank char[acter] on line"): "g,g",
             _("percent"): "percent",
@@ -89,7 +94,48 @@ class MotionRule(MappingRule):
             #  other  #
             ###########
             _("ex"): "colon",
+            ###########
+            #  marks  #
+            ###########
+            # TODO: tighten char to [a-vA-Z0-9]
+            _("mark <char>"): "backtick,%(char)s",
+            _("mark <char> first non-blank"): "apostrophe,%(char)s",
+            _("mark <char> [and] keep jumps"): "g,backtick,%(char)s",
+            _("mark <char> first non-blank [and] keep jumps"): "g,apostrophe,%(char)s",
+            _("first char[acter] of last (change|yank)"): "apostrophe,lbracket",
+            _("last char[acter] of last (change|yank)"): "apostrophe,rbracket",
+            _("start of last selection"): "apostrophe,langle",
+            _("end of last selection"): "apostrophe,rangle",
+            _("restore position"): "apostrophe,apostrophe",
+            _("restore position at last buffer exit"): "apostrophe,dquote",
+            _("restore position at last insert"): "apostrophe,caret",
+            _("restore position at last change"): "apostrophe,period",
+            _("first non-blank char[acater] of next lowercase mark"): "rbracket,apostrophe",
+            _("next lowercase mark"): "rbracket,backtick",
+            _("first non-blank char[acter] of previous lowercase mark"): "lbracket,apostrophe",
+            _("previous lowercase mark"): "lbracket,backtick",
+            #####################
+            #  various motions  #
+            #####################
+            _("(percent|match of next item)"): "percent",
+            _("previous unmatched (open|left) paren"): "lbracket,lparen",
+            _("previous unmatched (open|left) [curly] brace"): "lbracket,lbrace",
+            _("next unmatched (close|right) paren"): "rbracket,rparen",
+            _("next unmatched (close|right) [curly] brace"): "rbracket,rbrace",
+            _("next start of method"): "rbracket,m",
+            _("next end of method"): "rbracket,s-m",
+            _("previous start of method"): "lbracket,m",
+            _("previous end of method"): "lbracket,s-m",
+            _("previous unmatched macro"): "lbracket,hash",
+            _("next unmatched macro"): "rbracket,hash",
+            _("previous start of comment"): "lbracket,asterisk",
+            _("next end of comment"): "rbracket,asterisk",
+            _("line from top"): "s-h",
+            _("middle [of (window|screen)]"): "s-m",
+            _("line from bottom"): "s-l",
             }
+        self.extras = [
+            RuleRef(name='char', rule=AnyCharacter())]
 
         MappingRule.__init__(self, *args, **kwargs)
 
@@ -136,10 +182,9 @@ class MotionOperatorRule(CompoundRule):
     def __init__(self, *args, **kwargs):
         self.spec = _(
             "<operator> "
-            "(<line>|to "
+            "(<line>|[to] "
             "(<motion>|<operatormotion>) "
             "[<numbers>] "
-            "[into buffer <buffer>] "
             "[<mode> mode])")
         self.extras = [
             Choice(name='operator', choices={
@@ -165,9 +210,6 @@ class MotionOperatorRule(CompoundRule):
                 max=3),
             RuleRef(name='motion', rule=MotionRule()),
             RuleRef(name='operatormotion', rule=VisualMotionRule()),
-            Choice(name='buffer', choices={
-                _("default"): "$",
-                }),
             Choice(name='mode', choices={
                 _("character"): "v",
                 _("line"): "s-v",
@@ -242,55 +284,161 @@ class VimNormalRule(MappingRule):
 
     def __init__(self, *args, **kwargs):
         self.mapping = {
-            _("addition"): "c-a",
-            _("scroll back screen"): "c-b",
             _("interrupt"): "c-c",
+            _("display file name"): "c-g",
+            _("redraw screen"): "c-l",
+            _("suspend"): "c-z",
+            _("store and exit"): "s-z,s-z",
+            _("unsafe exit"): "s-z,s-q",
+            _("edit alternate"): "c-caret",
+            _("help"): "f1",
+            ###########
+            #  jumps  #
+            ###########
+            _("next jumplist"): "c-i",
+            _("previous jumplist"): "c-o",
+            _("previous changelist"): "g,semicolon",
+            _("next changlist"): "g,comma",
+            ###########
+            #  marks  #
+            ###########
+            _("set mark <char>"): "m,%(char)s",
+            _("set previous mark"): "m,apostrophe",
+            _("set (open|left) [square] bracket mark"): "m,lbracket",
+            _("set (close|right) [square] bracket mark"): "m,rbracket",
+            _("set (open|left) angle bracket mark"): "m,langle",
+            _("set (close|right) angle bracket mark"): "m,rangle",
+            ###############
+            #  scrolling  #
+            ###############
             _("scroll down half"): "c-d",
             _("scroll extra up"): "c-e",
             _("scroll forward screen"): "c-f",
-            _("display file name"): "c-g",
-            _("redraw screen"): "c-l",
-            _("jumplist newer"): "c-i",
-            _("jumplist older"): "c-o",
-            _("redo"): "c-r",
-            _("tag older"): "c-t",
-            _("scroll half up"): "c-u",
-            _("visual block"): "c-v",
-            _("window"): "c-w",
-            _("subtract"): "c-x",
+            _("scroll back screen"): "c-b",
             _("scroll down"): "c-y",
-            _("suspend"): "c-z",
+            _("scroll half up"): "c-u",
+            ###########
+            #  modes  #
+            ###########
+            _("insert"): "i",
             _("insertmode"): "c-backslash,c-g",
-            _("indent"): "c-rbracket",
-            _("edit alternate"): "c-caret",
-            _("cap append"): "s-a",
-            _("copy to end of line"): "s-c",
-            _("delete to end of line"): "s-d",
-            _("find char[acter] left"): "s-f",
-            _("goto line from top of screen"): "s-h",
             _("insert at start of line"): "s-i",
-            _("join lines"): "s-j",
-            _("lookup keyword"): "s-k",
-            _("goto line from bottom of screen"): "s-l",
-            _("goto middle of screen"): "s-m",
-            _("backwards next"): "s-n",
+            _("insert on newline"): "o",
             _("insert on newline before"): "s-o",
-            _("paste before"): "s-p",
-            _("external mode"): "s-q",
-            _("replace mode"): "s-r",
-            _("switch lines"): "s-s",
-            _("backwards move to char[acter]"): "s-t",
-            _("undo on line"): "s-u",
-            _("visual line"): "s-v",
-            _("backwards delete char[acter]"): "s-x",
-            _("yank line"): "s-y",
-            _("store and exit"): "s-z,s-z",
-            _("unsafe exit"): "s-z,s-q",
             _("append"): "a",
-            _("(insert|inner)"): "i",
+            _("(cap|big) append"): "s-a",
+            _("substitute"): "s",
+            _("replace [mode]"): "s-r",
+            _("external [mode]"): "s-q",
+            _("visual"): "v",
+            _("visual line"): "s-v",
+            _("visual block"): "c-v",
+            ######################
+            #  undo/redo/repeat  #
+            ######################
+            _("redo"): "c-r",
+            _("undo"): "u",
+            _("undo on line"): "s-u",
+            _("(repeat|period)"): "dot",
+            _("(at|repeat register <register>"): "at,%(register)s",
+            _("repeat previous register repeat"): "at,at",
+            _("repeat ex"): "at,colon",
+            _("(ampersand|repeat [last] (search|replace))"): "ampersand",
+            _("(semicolon|repeat (find|tag))"): "semicolon",
+            _("(comma|reverse repeat (find|tag))"): "comma",
+            _("record <register>"): "q,%(register)s",
+            _("stop recording"): "q",
+            _("edit ex"): "q,colon",
+            _("edit search"): "q,slash",
+            _("edit backward search"): "q,question",
+            ##################
+            #  text editing  #
+            ##################
+            _("addition"): "c-a",
+            _("subtract"): "c-x",
+            _("indent"): "c-rbracket",
+            _("join lines"): "s-j",
+            _("delete char[acter]"): "x",
+            _("backwards delete char[acter]"): "s-x",
+            _("replace <char>"): "r,%(char)s",
+            _("(tilde|switch case)"): "tilde",
+            ####################
+            #  copy and paste  #
+            ####################
+            _("copy to end of line"): "s-c",
+            _("paste"): "p",
+            _("paste before"): "s-p",
+            _("register <register>"): "dquote,%(register)s",
+            _("diff get"): "do",
+            _("diff put"): "dp",
+            ############
+            #  search  #
+            ############
+            _("lookup keyword"): "s-k",
+            _("backward next"): "s-n",
+            _("next"): "n",
+            _("after <char>"): "t,%(char)s",
+            _("backward move after <char>"): "s-t,%(char)s",
+            _("tag older"): "c-t",
+            _("(hash|backward search)"): "hash",
+            _("(asterisk|forward search)"): "asterisk",
+            _("(slash|search)"): "slash",
+            _("(question [mark]|backward search)"): "question",
+            ############
+            #  window  #
+            ############
+            _("window increase height"): "c-w,plus",
+            _("window decrease height"): "c-w,hyphen",
+            _("window increase width"): "c-w,rangle",
+            _("window decrease width"): "c-w,langle",
+            _("window equalise"): "c-w,equal",
+            _("window move left"): "c-w,s-h",
+            _("window move down"): "c-w,s-j",
+            _("window move up"): "c-w,s-k",
+            _("window move right"): "c-w,s-l",
+            _("window preview"): "c-w,s-p",
+            _("window rotate up"): "c-w,s-r",
+            _("window move to [new] tab"): "c-w,s-t",
+            _("window previous"): "c-w,s-w",
+            _("window split and jump"): "c-w,rbracket",
+            _("window split and edit alternate"): "c-w,caret",
+            _("window set height"): "c-w,underscore",
+            _("window bottom"): "c-w,b",
+            _("window close"): "c-w,c",
+            _("window split and jump to definition"): "c-w,d",
+            _("window split and edit file"): "c-w,f",
+            _("window split, edit file and jump"): "c-w,s-f",
+            # TODO: add c-w,g,] and c-w,g,}
+            _("window tab and edit file"): "c-w,g,f",
+            _("window tab, edit file and jump"): "c-w,g,s-f",
+            _("window left"): "c-w,h",
+            _("window split and jump to declaration"): "c-w,i",
+            _("window down"): "c-w,j",
+            _("window up"): "c-w,k",
+            _("window right"): "c-w,l",
+            _("window new"): "c-w,n",
+            _("window only"): "c-w,o",
+            _("window last"): "c-w,p",
+            _("window rotate"): "c-w,r",
+            _("window split [horizontal]"): "c-w,s",
+            _("window top"): "c-w,t",
+            _("window split vertical"): "c-w,v",
+            _("window next"): "c-w,w",
+            _("window exchange"): "c-w,x",
+            _("window close preview"): "c-w,z",
+            _("window width"): "c-w,bar",
+            _("window tag in preview"): "c-w,rbrace",
             }
+        self.extras = {
+            # TODO: tighten register to [a-zA-Zs-9.%#:-"]
+            RuleRef(name='register', rule=AnyCharacter()),
+            RuleRef(name='char', rule=AnyCharacter())}
 
         MappingRule.__init__(self, *args, **kwargs)
+
+####################################
+#  Rules for true vim normal mode  #
+####################################
 
 class TrueVimNormalRule(CompoundRule):
 
@@ -375,12 +523,13 @@ def load():
                 title=' VIM ') & \
              ProxyAppContext(
                  cls='terminator',
-                 title='mode:Insert'))))
+                 title='mode:Normal'))))
     TRUE_VIM_NORMAL_GRAMMAR.add_rule(
         TrueVimNormalRepetitionRule())
     TRUE_VIM_NORMAL_GRAMMAR.load()
 
     print 'vim grammars: Loaded.'
+    print TRUE_VIM_NORMAL_GRAMMAR.get_complexity_string()
 
 def unload():
     """Unregister grammar."""
